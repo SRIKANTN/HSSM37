1a881d0ce7fea18b6a485485c9241e5df7eced44
Pluto <Pluto@DESKTOP-JVK1AED>
2020-01-29 09:38:38 +0530

    2nd msg

---
+++ src/Com_ActiTime_Tests/Demo.java
@@ -0,0 +1,14 @@
+package Com_ActiTime_Tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void testa()
+	{
+		Reporter.log("hiiii",true);
+	}
+
+}
